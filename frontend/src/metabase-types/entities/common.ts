94d571a929977042f1c3623d4e37b37397e153b6
Romeo Van Snick <romeo@romeovansnick.be>
2026-05-01 16:11:45 +0200

    Move `setCollectionPreview` from entities into a RTK-based hook (#73460)
    * Move setCollectionPreview from objecActions to a custom RTK-based hook
    
    * Convert CollectionContent to RTK-query to avoid relying on entity caching
    
    * Update entity system when listing collection

--- frontend/src/metabase-types/entities/common.ts
+++ frontend/src/metabase-types/entities/common.ts
@@ -3,6 +3,5 @@ import type { Collection } from "metabase-types/api";
 export type WrappedEntity<Entity> = {
   getCollection: () => Collection;
   setCollection: (collection: Collection) => void;
-  setCollectionPreview: (isEnabled: boolean) => void;
   setPinned: (isPinned: boolean) => void;
 } & Entity;
