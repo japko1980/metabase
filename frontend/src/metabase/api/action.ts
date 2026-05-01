62592e850d0fbb756439d4763983a252a5da0b21
Romeo Van Snick <romeo@romeovansnick.be>
2026-05-01 22:48:06 +0200

    Move `{create|delete}PublicLink` to RTK-based hooks (#73508)
    * Move createPublicLink and deletePublicLink to use RTK hooks
    
    * Remove connect wrapper for useSelector

--- frontend/src/metabase/api/action.ts
+++ frontend/src/metabase/api/action.ts
@@ -117,6 +117,7 @@ export const {
   useListActionsQuery,
   useListPublicActionsQuery,
   useUpdateActionMutation,
+  useCreateActionPublicLinkMutation,
   useDeleteActionPublicLinkMutation,
   endpoints: {
     listPublicActions,
